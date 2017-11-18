8fafd5362ed1f31c1f1d8b9c8488eb4533bc391f
Matthias Kammüller <matthias@kammueller.eu>
2017-11-18 10:41:32 +0100

    started with DB integration

--- src/de/selectcode/hackaTUM/x17/haCarTUM/server/AzureAdapter.java
+++ src/de/selectcode/hackaTUM/x17/haCarTUM/server/AzureAdapter.java
@@ -5,11 +5,54 @@ import de.selectcode.hackaTUM.x17.haCarTUM.util.User;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
 /**
  * communication with the Azure Database & the AI
  * TODO
  */
 public class AzureAdapter {
+
+    public static void main(String[] args) {
+
+        // Connect to database
+        String hostName = "hacartum.database.windows.net";
+        String dbName = "trainingsdaten";
+        String user = "matthias";
+        String password = "Palladium$1";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT * FROM dbo.sensor_data";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getFloat(1) + " "
+                            + resultSet.getFloat(2) + " " +
+                            +resultSet.getFloat(4));
+                }
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     // TODO: uplink 'n' stuff
 
     public AzureAdapter() {
