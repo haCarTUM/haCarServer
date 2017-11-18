7a1715a0fcee088cee343909279d57da91fabc7a
Matthias Kammüller <matthias@kammueller.eu>
2017-11-18 05:50:03 +0100

    that's a first test

--- src/main/java/de/selectcode/hackaTUM/x17/haCarTUM/server/Main.java
+++ src/main/java/de/selectcode/hackaTUM/x17/haCarTUM/server/Main.java
@@ -1,11 +1,19 @@
 package de.selectcode.hackaTUM.x17.haCarTUM.server;
 
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
-        Web3j web3j = Web3j.build(new HttpService(/* TODO: URL */));
-        web3j.ethAccounts(); // TODO
+        Web3j web3 = Web3j.build(new HttpService(/* TODO: URL */));
+        try {
+            final Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();// TODO
+            System.out.println("Version " + web3ClientVersion);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
